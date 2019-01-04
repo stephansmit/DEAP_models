8c9cb29b1da0ae41131d364b19336beac986a33c
stephan <stephansmit@hotmail.com>
2019-01-04 13:14:40 +0100

    added the deap models

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+
 from deap import base
 from deap import creator
 from deap import tools
@@ -6,57 +7,76 @@ from functools import partial
 import numpy as np
 import pickle
 from deap import algorithms
+import random
+from datetime import datetime
+
+
+creator.create("FitnessMin", base.Fitness, weights=(1.0,))
+creator.create("Individual", np.ndarray, fitness=creator.FitnessMin)
 
 def objective(individual, generation):
     return random.random()+individual[0],
 
+def feasible(individual):
+    """Feasibility function for the individual. Returns True if feasible False
+    otherwise."""
+    return all(individual >0)
 
-creator.create("FitnessMin", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMin)
+def distance(individual):
+    """A distance function to the feasibility region."""
+    return (individual[0] - 5.0)**2
 
 
+#register individuals
 toolbox = base.Toolbox()
-toolbox.register("attr_bool", random.uniform, 0.90, 1.10)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 5)
+toolbox.register("attr_float", random.uniform, 0.90, 1.10)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, 5)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-# toolbox.register("map", pool.map)
 toolbox.register("mate", tools.cxTwoPoints)
 toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("evaluate", objective)
-pop = toolbox.population(n=30)
+toolbox.decorate("evaluate", tools.DeltaPenalty(feasible, -100.0))
+
+
 CXPB, MUTPB = 0.5, 1.0
 
+restart = True
+restart_file = "ga_restart.pkl"
+save_file = "ga_restart3.pkl"
 
 
-
-if False:
-    # A file name has been given, then load the data from the file
-    with open(checkpoint, "r") as cp_file:
+#restart from file
+if restart:
+    with open(restart_file, "r") as cp_file:
         cp = pickle.load(cp_file)
     population = cp["population"]
     start_gen = cp["generation"]
     halloffame = cp["halloffame"]
     logbook = cp["logbook"]
     random.setstate(cp["rndstate"])
+#fresh start
 else:
     population = toolbox.population(n=30)
     start_gen = 0
-    halloffame = tools.HallOfFame(maxsize=1)
+    halloffame = tools.HallOfFame(maxsize=1, similar=np.array_equal)
     logbook = tools.Logbook()
+    random.seed(datetime.now())
 
-
-
-stats = tools.Statistics(lambda ind: ind.fitness.values)
-stats.register("avg", np.mean)
-stats.register("std", np.std)
-stats.register("min", np.min)
-stats.register("max", np.max)
+#register stats
+stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+stats_ind = tools.Statistics(lambda ind: ind)
+mstats = tools.MultiStatistics(fitness=stats_fit, individual=stats_ind)
+mstats.register("avg", np.mean, axis=0)
+mstats.register("std", np.std, axis=0)
+mstats.register("min", np.min, axis=0)
+mstats.register("max", np.max, axis=0)
+mstats.register("size", np.size, axis=0)
 
 
 
 FREQ=1
-for gen in range(start_gen, 100):
+for gen in range(start_gen, start_gen+100):
     population = algorithms.varAnd(population, toolbox, cxpb=CXPB, mutpb=MUTPB)
 
     # Evaluate the individuals with an invalid fitness
@@ -66,19 +86,16 @@ for gen in range(start_gen, 100):
         ind.fitness.values = fit
 
     halloffame.update(population)
-    record = stats.compile(population)
+    record = mstats.compile(population)
     logbook.record(gen=gen, evals=len(invalid_ind), **record)
 
     population = toolbox.select(population, k=len(population))
-
     if gen % FREQ == 0:
         # Fill the dictionary using the dict(key=value[, ...]) constructor
         cp = dict(population=population, generation=gen, halloffame=halloffame,
                   logbook=logbook, rndstate=random.getstate())
 
-        with open("checkpoint_name.pkl", "wb") as cp_file:
+        with open(save_file, "wb") as cp_file:
             pickle.dump(cp, cp_file)
 
-
-for i in logbook:
-    print(i['avg'])
+print(np.size(logbook.chapters["fitness"].select('avg')))
--- plot.py
+++ plot.py
@@ -1,25 +1,65 @@
-
-gen = logbook.select("gen")
-fit_mins = logbook.chapters["fitness"].select("min")
-size_avgs = logbook.chapters["size"].select("avg")
+import pickle
+import random
+from deap import creator, base
+import numpy as np
 
 import matplotlib.pyplot as plt
+creator.create("FitnessMin", base.Fitness, weights=(1.0,))
+creator.create("Individual", np.ndarray, fitness=creator.FitnessMin)
+
+restart_file1='ga_restart2.pkl'
+restart_file2='ga_restart3.pkl'
+
+with open(restart_file1, "r") as cp_file:
+    cp = pickle.load(cp_file)
+population1 = cp["population"]
+start_gen1 = cp["generation"]
+halloffame1 = cp["halloffame"]
+logbook1 = cp["logbook"]
+random.setstate(cp["rndstate"])
+
+
+with open(restart_file2, "r") as cp_file:
+    cp = pickle.load(cp_file)
+population2 = cp["population"]
+start_gen2 = cp["generation"]
+halloffame2 = cp["halloffame"]
+logbook2 = cp["logbook"]
+random.setstate(cp["rndstate"])
+
+
+gen1 = logbook1.select("gen")
+fit_mins1 = logbook1.chapters["fitness"].select("min")
+size_avgs1 = logbook1.chapters["individual"].select("max")
+
+gen2 = logbook2.select("gen")
+fit_mins2 = logbook2.chapters["fitness"].select("min")
+size_avgs2 = logbook2.chapters["individual"].select("max")
+
+
+
 
 fig, ax1 = plt.subplots()
-line1 = ax1.plot(gen, fit_mins, "b-", label="Minimum Fitness")
+line1 = ax1.plot(gen1, fit_mins1, "b-",marker='x', label="Minimum Fitness")
+line2 = ax1.plot(gen2, fit_mins2, "r-", label="Minimum Fitness")
+
 ax1.set_xlabel("Generation")
-ax1.set_ylabel("Fitness", color="b")
-for tl in ax1.get_yticklabels():
-    tl.set_color("b")
+ax1.set_ylabel("Fitness")
+
+
+# for tl in ax1.get_yticklabels():
+#     tl.set_color("b")
 
 ax2 = ax1.twinx()
-line2 = ax2.plot(gen, size_avgs, "r-", label="Average Size")
-ax2.set_ylabel("Size", color="r")
-for tl in ax2.get_yticklabels():
-    tl.set_color("r")
+line12 = ax2.plot(gen1, size_avgs1, label="Average Size")
+line22 = ax2.plot(gen2, size_avgs2, marker='x', label="Average Size")
 
-lns = line1 + line2
-labs = [l.get_label() for l in lns]
-ax1.legend(lns, labs, loc="center right")
+ax2.set_ylabel("Individuals")
+# for tl in ax2.get_yticklabels():
+#     tl.set_color("r")
+
+# lns = line1 + line2
+# labs = [l.get_label() for l in lns]
+# ax1.legend(lns, labs, loc="center right")
 
 plt.show()
